aa0e5153ad0380d97c145513f6fd054fdbc98061
Aaryak Shah <aaryakdshah@gmail.com>
2022-03-06 01:03:09 +0530

    add: remote code execution

--- server/routes/code.js
+++ server/routes/code.js
@@ -1,83 +1,101 @@
 const express = require("express")
 const CONSTANTS = require("../constants")
+const User = require("../db/schema/user")
 const router = express.Router()
+const jwt = require("../utils/jwt")
+const fs = require("fs")
+const pythonHandler = require("../languages/python")
+const cppHandler = require("../languages/cpp")
+const jsHandler = require("../languages/javascript")
 
-router.post("/", (req, res) => {
-  // request body destructured
-  const {
-    language, // specifies programming language, doubles as file extension for storage/code.xyz file
-    input, // user's input string to be stored in storage/input
-    code, // user's code string to be stored in storage/code.xyz
-  } = req.body
+router.post("/", async (req, res) => {
+  try {
+    // request body destructured
+    const {
+      language, // specifies programming language, doubles as file extension for storage/code.xyz file
+      input, // user's input string to be stored in storage/input
+      code, // user's code string to be stored in storage/code.xyz
+    } = req.body
 
-  const uid = "" // TODO: get key from user jwt and mongodb query
-  const userFilePath = CONSTANTS.USERSTORAGEPATH + uid
-
-  // storage for input
-  fs.writeFile(userFilePath, input, (inpFileErr) => {
-    if (inpFileErr) {
-      res.send({ stderr: `input err ${inpFileErr}` })
-      return
+    const token = req.cookies["token"]
+    const { uid, role } = jwt.verify(token)
+    try {
+      await User.findById(uid)
+    } catch (e) {
+      console.error(e)
+      res.status(400).json({ data: null, error: "User does not exist" })
     }
+    const userFilePath = CONSTANTS.USERSTORAGEPATH + uid
 
-    // storage for user's program
-    fs.writeFile(userFilePath + "." + language, code, async (codeFileErr) => {
-      if (codeFileErr) {
-        res.send({ stderr: `code err ${codeFileErr}` })
+    // storage for input
+    fs.writeFile(userFilePath, input, (inpFileErr) => {
+      if (inpFileErr) {
+        res.send({ stderr: `input err ${inpFileErr}` })
         return
       }
 
-      // switching logic to call language specific handler
-      switch (language) {
-        // python
-        case "py": {
-          pythonHandler(uid, USERSTORAGEPATH)
-            .then((result) => {
-              const { stdout, stderr, code, signal } = result
-              const err_send = stderr || signal || code
-              if (err_send) {
-                res.send({ output: err_send, verdict: "error" })
-              }
-              res.send({ output: stdout, verdict: "success" })
-            }) // callback responds result of successful execution
-            .catch((err) => res.send(err)) // callback responds result of failed execution
-          break
+      // storage for user's program
+      fs.writeFile(userFilePath + "." + language, code, async (codeFileErr) => {
+        if (codeFileErr) {
+          res.send({ stderr: `code err ${codeFileErr}` })
+          return
         }
 
-        // c++
-        case "cpp": {
-          cppHandler(uid, USERSTORAGEPATH)
-            .then((result) => {
-              const { stdout, stderr, code, signal } = result
-              const err_send = stderr || signal || code
-              if (err_send) {
-                res.send({ output: err_send, verdict: "error" })
-              }
-              res.send({ output: stdout, verdict: "success" })
-            }) // callback responds result of successful execution
-            .catch((err) => {
-              res.send(err)
-            }) // callback responds result of failed execution
-          break
-        }
+        // switching logic to call language specific handler
+        switch (language) {
+          // python
+          case "py": {
+            pythonHandler(uid, CONSTANTS.USERSTORAGEPATH)
+              .then((result) => {
+                const { stdout, stderr, code, signal } = result
+                const err_send = stderr || signal || code
+                if (err_send) {
+                  res.send({ output: err_send, verdict: "error" })
+                }
+                res.send({ output: stdout, verdict: "success" })
+              }) // callback responds result of successful execution
+              .catch((err) => res.send(err)) // callback responds result of failed execution
+            break
+          }
 
-        // node.js
-        case "js": {
-          jsHandler(uid, USERSTORAGEPATH)
-            .then((result) => {
-              const { stdout, stderr, code, signal } = result
-              const err_send = stderr || signal || code
-              if (err_send) {
-                res.send({ output: err_send, verdict: "error" })
-              }
-              res.send({ output: stdout, verdict: "success" })
-            }) // callback responds result of successful execution
-            .catch((err) => res.send(err)) // callback responds result of failed execution
-          // break
+          // c++
+          case "cpp": {
+            cppHandler(uid, CONSTANTS.USERSTORAGEPATH)
+              .then((result) => {
+                const { stdout, stderr, code, signal } = result
+                const err_send = stderr || signal || code
+                if (err_send) {
+                  res.send({ output: err_send, verdict: "error" })
+                }
+                res.send({ output: stdout, verdict: "success" })
+              }) // callback responds result of successful execution
+              .catch((err) => {
+                res.send(err)
+              }) // callback responds result of failed execution
+            break
+          }
+
+          // node.js
+          case "js": {
+            jsHandler(uid, CONSTANTS.USERSTORAGEPATH)
+              .then((result) => {
+                const { stdout, stderr, code, signal } = result
+                const err_send = stderr || signal || code
+                if (err_send) {
+                  res.send({ output: err_send, verdict: "error" })
+                }
+                res.send({ output: stdout, verdict: "success" })
+              }) // callback responds result of successful execution
+              .catch((err) => res.send(err)) // callback responds result of failed execution
+            // break
+          }
         }
-      }
+      })
     })
-  })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ data: null, error: "Internal server error" })
+  }
 })
 
 module.exports = router
